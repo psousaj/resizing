a92cc5fc7d8faf8f4a9ca3b98dfcab98aa130940
José <josepsousa2012@gmail.com>
2023-04-19 17:56:08 -0300

    feat - functional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,36 @@
 import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
-  const [posX, setPosX] = useState(0);
-  const [isMouseDown, setIsMouseDown] = useState(false);
-  const [initialPosX, setInitialPosX] = useState(0);
-  const [asideWidth, setAsideWidth] = useState(8);
+  const [isResizing, setIsResizing] = useState(false);
+  const [width, setWidth] = useState(120);
+  const [initialMouseX, setMouseX] = useState(0);
+
+  const minWidth = 120;
+  const maxWidth = 250;
 
   const asideRef = useRef(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   function handleMouseDown(event: React.MouseEvent) {
-    if (asideRef.current) {
-      setIsMouseDown(true);
-      setInitialPosX(event.clientX - posX);
-    }
+    setIsResizing(true);
+    setMouseX(event.clientX);
   }
 
   function handleMouseUp() {
-    setIsMouseDown(false);
+    setIsResizing(false);
+    setMouseX(0);
   }
 
   function handleMouseMove(event: React.MouseEvent) {
-    if (isMouseDown) {
-      const newPosX = event.clientX - initialPosX;
-      setPosX(newPosX);
-
-      const newAsideWidth = newPosX + 10;
-      setAsideWidth(newAsideWidth);
+    if (isResizing && event.pageX > initialMouseX) {
+      const newWidth = event.pageX;
+      if (newWidth >= minWidth && newWidth <= maxWidth) {
+        setWidth(newWidth);
+      } else if (newWidth < minWidth) {
+        setWidth(minWidth);
+      } else if (newWidth > maxWidth) {
+        setWidth(maxWidth);
+      }
     }
   }
 
@@ -39,13 +44,13 @@ export default function Home() {
     >
       <aside
         ref={asideRef}
-        style={{ width: `${asideWidth}rem` }}
+        style={{ width: `${width}px` }}
         className="relative left-0 top-0 bg-red-500"
       ></aside>
       <div
+        ref={divRef}
         onMouseDown={handleMouseDown}
-        style={{ left: `${posX}px` }}
-        className="z-10 relative left-0 top-0 bg-green-500 w-10"
+        className="resizing z-10 relative left-0 top-0 bg-green-500 w-10"
       ></div>
       <main className="relative flex-1 bg-blue-500"></main>
     </div>
